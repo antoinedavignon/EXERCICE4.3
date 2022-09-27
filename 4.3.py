df5958a68cf182569755228a2559ccc9ff155f5b
2249961 <2249961@cstjean.qc.ca>
2022-09-27 10:21:02 -0400

    version 3 amélioration

--- 4.3.py
+++ 4.3.py
@@ -1,19 +1,15 @@
 # Auteur : Manu
 
-
-#CONSTANTES
-MONTANT_FIXE = 1.50 # en $
-
 # VARIABLES
-facture_sans_pourboire : float
-facture_totale : float
-pourboire : float
+facture_sans_pourboire : float  # en $
+pourboire : float   # en $
 
 facture_sans_pourboire = float(input("Quel est le total de votre facture ? : "))
-
+# calcul du pourboire selon le prix total.
 
 if facture_sans_pourboire < 0:
     print("Impossible vérifier votre réponse.")
+    exit()
 
 elif facture_sans_pourboire < 10:
     pourboire = 1.5
@@ -24,6 +20,4 @@ elif facture_sans_pourboire < 100:
 elif 200 > facture_sans_pourboire >= 100:
     pourboire = 15 + (0.05 * facture_sans_pourboire)
 
-else:
-    pourboire = 0
-    print(f"La facture totale avec pourboire est de {facture_sans_pourboire + pourboire:.2f}$ avec un pourboire de {pourboire:.2f}$.")
+print(f"La facture totale avec pourboire est de {facture_sans_pourboire + pourboire:.2f}$ avec un pourboire de {pourboire:.2f}$.")
